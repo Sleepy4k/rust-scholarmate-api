4f904fa90e2d50730b8d41fb67d3218756bf00e5
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-12 20:50:08 +0700

    chore: auth helper

--- src/helpers/auth.rs
+++ src/helpers/auth.rs
@@ -1,5 +1,6 @@
 use argon2::{self, Config};
 
+#[doc = "Salt and hash a password"]
 pub fn hash_password(password: &str) -> String {
   let config = Config::default();
   let salt = b"mermoauthhash";
@@ -9,6 +10,7 @@ pub fn hash_password(password: &str) -> String {
   hash
 }
 
+#[doc = "Verify a password against a hash"]
 pub fn verify_password(password: &str, hashed_password: &str) -> bool {
   argon2::verify_encoded(hashed_password, password.as_bytes()).unwrap_or(false)
 }
