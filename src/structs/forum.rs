0ddfd069f0c56b1f3abd1bfc036c83718d1e4923
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-13 14:52:01 +0700

    feat: forum struct

---
+++ src/structs/forum.rs
@@ -0,0 +1,9 @@
+use serde::{Serialize};
+
+#[doc = "Forum struct"]
+#[derive(Debug, Serialize)]
+pub struct ForumStruct {
+  pub id: i32,
+  pub name: String,
+  pub message: String,
+}
--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,11 +1,13 @@
 pub mod application;
 pub mod auth;
+pub mod forum;
 pub mod schoolarship;
 pub mod student;
 pub mod university;
 
 pub use application::*;
 pub use auth::*;
+pub use forum::*;
 pub use schoolarship::*;
 pub use student::*;
 pub use university::*;
