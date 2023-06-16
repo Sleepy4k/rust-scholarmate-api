101a2c102fbd19e10a8b56f843a63eb70427c921
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-16 23:02:21 +0700

    chore: some struct

--- src/structs/application_struct.rs
+++ src/structs/application_struct.rs
@@ -7,12 +7,14 @@ pub struct ApplicationStruct {
   pub student_id: i32,
   pub univ_id: i32,
   pub status: String,
+  pub major: String,
 }
 
 #[doc = "Detail application struct"]
 #[derive(Debug, Serialize)]
 pub struct DetailApplicationStruct {
   pub id: i32,
+  pub univ_id: i32,
   pub name: String,
   pub major: String,
   pub image: String,
--- src/structs/schoolarship_struct.rs
+++ src/structs/schoolarship_struct.rs
@@ -9,4 +9,20 @@ pub struct SchoolarshipStruct {
   pub major: String,
   pub quantity: i32,
   pub requirement: String,
+  pub univ_id: i32,
+}
+
+#[doc = "Detail schoolarship struct"]
+#[derive(Debug, Serialize)]
+pub struct DetailSchoolarshipStruct {
+  pub id: i32,
+  pub name: String,
+  pub description: String,
+  pub major: String,
+  pub quantity: i32,
+  pub requirement: String,
+  pub univ_id: i32,
+  pub univ_name: String,
+  pub univ_alias: String,
+  pub univ_description: String,
 }
