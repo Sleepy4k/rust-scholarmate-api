216811af6d38cc03ff568e165cb0b25e31b82662
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-19 17:29:00 +0700

    chore: some struct

--- src/structs/auth_struct.rs
+++ src/structs/auth_struct.rs
@@ -1,7 +1,5 @@
 use serde::{Serialize, Deserialize};
 
-use crate::structs::student_struct::StudentStruct;
-
 #[doc = "Token struct"]
 #[derive(Debug, Deserialize, Serialize)]
 pub struct TokenStruct {
@@ -10,36 +8,4 @@ pub struct TokenStruct {
   pub email: String,
   pub iat: u64,
   pub exp: u64,
-}
-
-#[doc = "User struct"]
-#[derive(Debug, Deserialize, Serialize)]
-pub struct UserStruct {
-  pub id: i32,
-  pub email: String,
-  pub role: String,
-}
-
-#[doc = "Detail user struct"]
-#[derive(Debug, Serialize)]
-pub struct DetailUserStruct {
-  pub id: i32,
-  pub email: String,
-  pub role: String,
-  pub student: Option<StudentStruct>,
-}
-
-#[doc = "Login struct"]
-#[derive(Debug, Deserialize, Serialize)]
-pub struct LoginStruct {
-  pub email: String,
-  pub password: String,
-}
-
-#[doc = "Register struct"]
-#[derive(Debug, Deserialize, Serialize)]
-pub struct RegisterStruct {
-  pub email: String,
-  pub password: String,
-  pub role: String,
 }
--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,15 +1,5 @@
-pub mod application_struct;
 pub mod auth_struct;
-pub mod forum_struct;
 pub mod main_struct;
-pub mod schoolarship_struct;
-pub mod student_struct;
-pub mod university_struct;
 
-pub use application_struct::*;
 pub use auth_struct::*;
-pub use forum_struct::*;
 pub use main_struct::*;
-pub use schoolarship_struct::*;
-pub use student_struct::*;
-pub use university_struct::*;
