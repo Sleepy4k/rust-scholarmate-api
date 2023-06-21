34ed6aa0e46de9308aeb2326962020d6f8f474cf
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-21 15:52:44 +0700

    chore: main lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,20 @@
 pub mod routes;
+pub mod models;
 pub mod helpers;
 pub mod structs;
-pub mod models;
 pub mod schemas;
 pub mod middlewares;
 pub mod controllers;
+pub mod repositories;
 
 pub use routes::*;
+pub use models::*;
 pub use helpers::*;
 pub use structs::*;
-pub use models::*;
 pub use schemas::*;
 pub use middlewares::*;
 pub use controllers::*;
+pub use repositories::*;
+
+#[cfg(test)]
+pub mod tests;
