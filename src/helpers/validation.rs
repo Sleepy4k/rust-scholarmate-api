6b940c2cf9c9b012ad62c36e2b926c9b2917b773
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-26 03:14:20 +0700

    chore: validation helper

--- src/helpers/validation.rs
+++ src/helpers/validation.rs
@@ -1,4 +1,5 @@
 use serde_json::Value;
+use chrono::prelude::*;
 use std::any::type_name;
 
 pub trait IsEmpty {
@@ -41,4 +42,28 @@ pub fn check_type<T>(_value: T) -> String {
   println!("type of value: {}", type_name);
 
   type_name
+}
+
+#[doc = "Check if number is positif"]
+pub fn check_if_positif_number(number: i32) -> bool {
+  number > 0
+}
+
+#[doc = "Check if date is valid"]
+pub fn check_if_valid_date(date: &str) -> bool {
+  let date = date.parse::<NaiveDate>();
+
+  match date {
+    Ok(result) => {
+      let current_date = Utc::now().naive_utc().date();
+
+      result <= current_date
+    },
+    Err(_) => false,
+  }
+}
+
+#[doc = "Check if role is valid"]
+pub fn check_if_valid_role(role: &str) -> bool {
+  role == "admin" || role == "user"
 }
