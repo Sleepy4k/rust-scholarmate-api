e35eefb382233756e27bc331902adc58ca7e3f60
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-07-05 06:18:24 +0700

    chore: auth controller

--- src/controllers/auth_controller.rs
+++ src/controllers/auth_controller.rs
@@ -1,8 +1,7 @@
 use serde_json::Value;
 use validator::Validate;
 use actix_web::{web, Responder};
-use std::{env, time::{SystemTime, UNIX_EPOCH}};
-use jsonwebtoken::{encode, Header, EncodingKey};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 use crate::{
   models::auth_model::*,
@@ -30,8 +29,8 @@ pub async fn login(state: web::Data<AppState>, body: web::Json<LoginSchema>) -> 
     let data = Value::from(validate_form.err().unwrap().to_string());
 
     return response_json(
-      "failed".to_string(),
-      "Please fill all fields".to_string(),
+      String::from("failed"),
+      String::from("please fill all fields"),
       vec![data]
     )
   }
@@ -39,16 +38,16 @@ pub async fn login(state: web::Data<AppState>, body: web::Json<LoginSchema>) -> 
   let user = match fetch_user_data_by_email(state.db.clone(), body.email.to_owned()).await {
     Some(user) => user,
     None => return response_json(
-      "failed".to_string(),
-      "Account not found".to_string(),
+      String::from("failed"),
+      String::from("account not found"),
       vec![]
     )
   };
 
   if !verify_password(body.password.as_str(), &user.password) {
     return response_json(
-      "failed".to_string(),
-      "Email or password is wrong".to_string(),
+      String::from("failed"),
+      String::from("email or password is wrong"),
       vec![]
     )
   }
@@ -66,10 +65,7 @@ pub async fn login(state: web::Data<AppState>, body: web::Json<LoginSchema>) -> 
     exp: token_time.saturating_add(604800),
   };
 
-  let jwt_secret = env::var("JWT_TOKEN_SECRET").unwrap_or_else(|_| String::from("secret"));
-  let key = EncodingKey::from_secret(jwt_secret.as_ref());
-  let token = encode(&Header::default(), &token_value, &key).unwrap_or_else(|_| String::new());
-
+  let token = token_value.to_jwt();
   let student = fetch_student_data_by_exists_column(state.db.clone(), user.email.to_owned(), String::new(), String::new()).await;
 
   let detail_user = convert_vec_to_values(vec![
@@ -82,8 +78,8 @@ pub async fn login(state: web::Data<AppState>, body: web::Json<LoginSchema>) -> 
   ]);
 
   response_json_with_cookie(
-    "success".to_string(),
-    "Successfully logged in".to_string(),
+    String::from("success"),
+    String::from("successfully logged in"),
     detail_user,
     token
   )
@@ -97,16 +93,16 @@ pub async fn register(state: web::Data<AppState>, body: web::Json<RegisterSchema
     let data = Value::from(validate_form.err().unwrap().to_string());
 
     return response_json(
-      "failed".to_string(),
-      "Please fill all fields".to_string(),
+      String::from("failed"),
+      String::from("please fill all fields"),
       vec![data]
     )
   }
 
   match fetch_user_data_by_email(state.db.clone(), body.email.to_owned()).await {
     Some(_) => return response_json(
-      "failed".to_string(),
-      "Email already exists".to_string(),
+      String::from("failed"),
+      String::from("email already exists"),
       vec![]
     ),
     None => ()
@@ -114,16 +110,16 @@ pub async fn register(state: web::Data<AppState>, body: web::Json<RegisterSchema
 
   if body.password.len() < 8 {
     return response_json(
-      "failed".to_string(),
-      "Password must be at least 8 characters".to_string(),
+      String::from("failed"),
+      String::from("password must be at least 8 characters"),
       vec![]
     )
   }
 
   if body.password != body.password_confirmation {
     return response_json(
-      "failed".to_string(),
-      "Password and confirm password must be same".to_string(),
+      String::from("failed"),
+      String::from("password and confirm password must be same"),
       vec![]
     )
   }
@@ -133,15 +129,15 @@ pub async fn register(state: web::Data<AppState>, body: web::Json<RegisterSchema
   let user_data = RegisterSchema {
     email: body.email.to_owned(),
     role: body.role.to_owned(),
-    password: hashed_password.to_owned(),
-    password_confirmation: hashed_password.to_owned(),
+    password: hashed_password,
+    password_confirmation: String::new(),
   };
 
   let data = insert_user_data(state.db.clone(), user_data).await;
 
   response_json(
-    "success".to_string(),
-    "Successfully registered".to_string(),
+    String::from("success"),
+    String::from("successfully registered"),
     data
   )
 }
@@ -149,9 +145,9 @@ pub async fn register(state: web::Data<AppState>, body: web::Json<RegisterSchema
 #[doc = "Logout user"]
 pub async fn logout() -> impl Responder {
   response_json_with_cookie(
-    "success".to_string(),
-    "Successfully logged out".to_string(),
+    String::from("success"),
+    String::from("successfully logged out"),
     vec![],
-    "".to_string()
+    String::new()
   )
 }
