6b057adf38fcdd1d3b2999eba082e91a11225517
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-07-09 03:03:59 +0700

    chore: auth model

--- src/models/auth_model.rs
+++ src/models/auth_model.rs
@@ -1,29 +1,9 @@
 use serde::{Serialize, Deserialize};
 
-use crate::models::student_model::StudentModel;
-
-#[doc = "User model"]
-#[derive(Debug, Deserialize, Serialize)]
-pub struct UserModel {
-  pub id: i32,
-  pub email: String,
-  pub role: String,
-  pub password: String,
-}
-
 #[doc = "Filtered user model"]
 #[derive(Debug, Deserialize, Serialize)]
 pub struct FilteredUserModel {
   pub id: i32,
   pub email: String,
   pub role: String,
-}
-
-#[doc = "Detail user model"]
-#[derive(Debug, Serialize)]
-pub struct DetailUserModel {
-  pub id: i32,
-  pub email: String,
-  pub role: String,
-  pub student: Option<StudentModel>,
 }
