36b676b345904a8f219e2fa72ead6696a2a7684a
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-07-09 03:04:28 +0700

    feat!: auth schema

--- src/schemas/mod.rs
+++ src/schemas/mod.rs
@@ -1,11 +1,9 @@
 pub mod application_schema;
-pub mod auth_schema;
 pub mod scholarship_schema;
 pub mod student_schema;
 pub mod university_schema;
 
 pub use application_schema::*;
-pub use auth_schema::*;
 pub use scholarship_schema::*;
 pub use student_schema::*;
 pub use university_schema::*;
