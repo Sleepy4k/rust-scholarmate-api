9d74d47153d2da5997dd0c119cf08a3ffb565ab1
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-07-15 12:25:02 +0700

    chore: chat schema

--- src/bin/ws_service/schemas/chat_schema.rs
+++ src/bin/ws_service/schemas/chat_schema.rs
@@ -1,10 +1,12 @@
 use serde::{Serialize, Deserialize};
 
+#[doc = "GeneralChatSchema is the struct that will be used to general chat schema in websocket."]
 #[derive(Debug, Deserialize, Serialize)]
 pub struct GeneralChatSchema {
   pub uid: i32
 }
 
+#[doc = "DetailChatSchema is the struct that will be used to detail chat schema in websocket."]
 #[derive(Debug, Deserialize, Serialize)]
 pub struct DetailChatSchema {
   pub name: String,
