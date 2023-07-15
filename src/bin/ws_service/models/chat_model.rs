792c06d2bb6ec0ae107cee2b0344ca48023682f0
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-07-15 12:24:22 +0700

    chore: some model

--- src/bin/ws_service/models/chat_model.rs
+++ src/bin/ws_service/models/chat_model.rs
@@ -1,6 +1,10 @@
 use chrono::NaiveDate;
+use serde_json::Value;
 use serde::{Deserialize, Serialize};
 
+use crate::models::room_model::RoomModel;
+
+#[doc = "ChatModel is the struct that will be used to chat model in websocket."]
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct ChatModel {
   pub id: i32,
@@ -10,9 +14,18 @@ pub struct ChatModel {
   pub created_at: NaiveDate,
 }
 
+#[doc = "NewChatModel is the struct that will be used to new chat model in websocket."]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct NewChatModel {
   pub user_id: i32,
   pub room_id: i32,
   pub message: String,
+}
+
+#[doc = "ChatResponseModel is the struct that will be used to chat response model in websocket."]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ChatResponseModel {
+  pub chats: Vec<Value>,
+  pub room: RoomModel,
+  pub users: Vec<Value>,
 }
--- src/bin/ws_service/models/room_model.rs
+++ src/bin/ws_service/models/room_model.rs
@@ -2,6 +2,7 @@ use chrono::NaiveDate;
 use serde_json::Value;
 use serde::{Deserialize, Serialize};
 
+#[doc = "RoomModel is the struct that will be used to room model in websocket."]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct RoomModel {
   pub id: i32,
@@ -11,12 +12,14 @@ pub struct RoomModel {
   pub created_at: NaiveDate,
 }
 
+#[doc = "NewRoomModel is the struct that will be used to new room model in websocket."]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct NewRoomModel {
   pub name: String,
   pub members: String,
 }
 
+#[doc = "RoomResponseModel is the struct that will be used to room response model in websocket."]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct RoomResponseModel {
   pub room: RoomModel,
