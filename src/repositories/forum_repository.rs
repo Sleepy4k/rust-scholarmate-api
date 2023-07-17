f2e2931c595a3a943066910c72da7ccd00062ddf
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-07-17 20:30:21 +0700

    chore: forum repository

--- src/repositories/forum_repository.rs
+++ src/repositories/forum_repository.rs
@@ -1,23 +1,25 @@
-use serde_json::Value;
 use sqlx::{Pool, Postgres};
 
 use crate::{
-  models::forum_model::*,
-  helpers::parse::convert_vec_to_values
+  enums::error_enum::ErrorEnum,
+  models::forum_model::ForumModel
 };
 
-#[doc = "Fetch all forum data"]
-pub async fn fetch_forum_data(pool: Pool<Postgres>, id: i32) -> Vec<Value> {
-  let data = sqlx::query_as!(ForumModel,
+#[doc = "Find forum data by id."]
+pub async fn find_forum_data(pool: Pool<Postgres>, id: i32) -> anyhow::Result<Vec<ForumModel>, ErrorEnum> {
+  match sqlx::query_as!(ForumModel,
     "select universities.alias, forums.message, universities.image, universities.link from forums
       join universities on forums.univ_id = universities.id where student_id = $1
       order by forums.id desc"
     , id)
-    .fetch_all(&pool)
-    .await
-    .unwrap();
-
-  let result = convert_vec_to_values(data);
-
-  result
+    .fetch_optional(&pool)
+    .await {
+      Ok(optional_data) => {
+        match optional_data {
+          Some(data) => Ok(vec![data]),
+          None => Ok(vec![])
+        }
+      },
+      Err(_) => Err(ErrorEnum::InternalServerError)
+    }
 }
