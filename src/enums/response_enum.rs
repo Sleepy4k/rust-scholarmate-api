47a671fa4637556da3951574ca8a4b67d2ad3c24
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-07-17 20:26:03 +0700

    chore: response enum

--- src/enums/response_enum.rs
+++ src/enums/response_enum.rs
@@ -3,22 +3,37 @@ use serde::{Serialize, Deserialize};
 
 #[doc = "Response enum for data"]
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum ResponseDataEnum<T> {
+pub enum ResponseDataEnum {
   SingleValue(Value),
   ArrayValue(Vec<Value>),
-  SingleObject(T),
-  ArrayObject(Vec<T>),
   ArrayUsize(Vec<usize>),
 }
 
-impl <T: Serialize> ResponseDataEnum<T> {
+impl ResponseDataEnum {
+  #[doc = "Get value from enum"]
   pub fn get_value(&self) -> Value {
     match self {
       ResponseDataEnum::SingleValue(value) => value.clone(),
       ResponseDataEnum::ArrayValue(value) => Value::Array(value.clone()),
-      ResponseDataEnum::SingleObject(value) => serde_json::to_value(value).unwrap(),
-      ResponseDataEnum::ArrayObject(value) => serde_json::to_value(value).unwrap(),
-      ResponseDataEnum::ArrayUsize(value) => Value::Array(value.iter().map(|x| Value::Number(serde_json::Number::from_f64(*x as f64).unwrap())).collect()),
+      ResponseDataEnum::ArrayUsize(value) => Value::Array(value.iter().map(|x| Value::Number(serde_json::Number::from_f64(*x as f64).unwrap())).collect())
+    }
+  }
+
+  #[doc = "Get array from enum"]
+  pub fn get_array(&self) -> Vec<Value> {
+    match self {
+      ResponseDataEnum::SingleValue(value) => vec![value.clone()],
+      ResponseDataEnum::ArrayValue(value) => value.clone(),
+      ResponseDataEnum::ArrayUsize(value) => value.iter().map(|x| Value::Number(serde_json::Number::from_f64(*x as f64).unwrap())).collect()
+    }
+  }
+
+  #[doc = "Check if value is empty"]
+  pub fn is_empty(&self) -> bool {
+    match self {
+      ResponseDataEnum::SingleValue(value) => value.is_null(),
+      ResponseDataEnum::ArrayValue(value) => value.is_empty(),
+      ResponseDataEnum::ArrayUsize(value) => value.is_empty()
     }
   }
 }
