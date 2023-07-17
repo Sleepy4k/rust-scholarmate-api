714289fdb19f6a2bcf721a7754185f046b9a9551
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-07-17 20:25:54 +0700

    feat: error enum

---
+++ src/enums/error_enum.rs
@@ -0,0 +1,33 @@
+use serde::{Serialize, Deserialize};
+
+#[doc = "Response enum for error"]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum ErrorEnum {
+  DatabaseError,
+  NotFoundError,
+  InternalServerError,
+  BadRequestError,
+  UnauthorizedError,
+  ForbiddenError,
+  UnprocessableEntityError,
+  ConflictError,
+  ValidationError,
+  CustomError,
+}
+
+impl ErrorEnum {
+  pub fn get_error(&self) -> String {
+    match self {
+      ErrorEnum::DatabaseError => String::from("database error"),
+      ErrorEnum::NotFoundError => String::from("not found error"),
+      ErrorEnum::InternalServerError => String::from("internal server error"),
+      ErrorEnum::BadRequestError => String::from("bad request error"),
+      ErrorEnum::UnauthorizedError => String::from("unauthorized error"),
+      ErrorEnum::ForbiddenError => String::from("forbidden error"),
+      ErrorEnum::UnprocessableEntityError => String::from("unprocessable entity error"),
+      ErrorEnum::ConflictError => String::from("conflict error"),
+      ErrorEnum::ValidationError => String::from("validation error"),
+      ErrorEnum::CustomError => String::from("custom error"),
+    }
+  }
+}
--- src/enums/mod.rs
+++ src/enums/mod.rs
@@ -1,3 +1,5 @@
+pub mod error_enum;
 pub mod response_enum;
 
+pub use error_enum::*;
 pub use response_enum::*;
