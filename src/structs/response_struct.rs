7c3ddd347ecaa00f722a839be561d2a865f4ed4c
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-07-17 20:29:41 +0700

    chore: response struct

--- src/structs/response_struct.rs
+++ src/structs/response_struct.rs
@@ -11,6 +11,16 @@ pub struct ResponseStruct {
 
 #[doc = "Response struct with token"]
 #[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ResponseStructWithToken {
+  pub status: String,
+  pub token: String,
+  pub message: String,
+  pub data: Value,
+}
+
+#[doc = "Response struct with token"]
+#[deprecated(note = "Please use ResponseStructWithToken instead")]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ResponseCookieStruct {
   pub status: String,
   pub message: String,
