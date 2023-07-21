ffacc80c05f0b00850e88f7d75a8ad99871cff82
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-07-21 21:18:01 +0700

    chore: some repository

--- src/bin/auth_service/repositories/auth_repository.rs
+++ src/bin/auth_service/repositories/auth_repository.rs
@@ -1,4 +1,3 @@
-use serde_json::Value;
 use sqlx::{Pool, Postgres};
 
 use crate::{
@@ -7,42 +6,50 @@ use crate::{
 };
 
 use scholarmate_api::{
-  models::auth_model::FilteredUserModel,
-  helpers::parse::convert_vec_to_values
+  enums::error_enum::ErrorEnum,
+  models::auth_model::FilteredUserModel
 };
 
-#[doc = "Insert new user"]
-pub async fn insert_user_data(pool: Pool<Postgres>, body: RegisterSchema) -> Vec<Value> {
-  let data = sqlx::query_as!(FilteredUserModel,
+#[doc = "Create an user data."]
+pub async fn create_user_data(pool: Pool<Postgres>, body: RegisterSchema) -> anyhow::Result<FilteredUserModel, ErrorEnum> {
+  match sqlx::query_as!(FilteredUserModel,
     "insert into users (email, password, role, verified) values ($1, $2, $3, $4) returning id, email, role",
     body.email, body.password, String::from("user"), false)
-    .fetch_all(&pool)
-    .await
-    .unwrap();
-
-  let result = convert_vec_to_values(data);
-
-  result
+    .fetch_one(&pool)
+    .await {
+      Ok(data) => Ok(data),
+      Err(_) => Err(ErrorEnum::InternalServerError)
+    }
 }
 
-#[doc = "Fetch user data by email"]
-pub async fn fetch_user_data_by_email(pool: Pool<Postgres>, email: String) -> Option<UserModel> {
-  let data = sqlx::query_as!(UserModel, "select * from users where email = $1", email)
+#[doc = "Find a user data by email."]
+pub async fn find_user_data_by_email(pool: Pool<Postgres>, email: String) -> anyhow::Result<UserModel, ErrorEnum> {
+  match sqlx::query_as!(UserModel, "select * from users where email = $1 limit 1", email)
     .fetch_optional(&pool)
-    .await
-    .unwrap();
-
-  data
+    .await {
+      Ok(optional_data) => {
+        match optional_data {
+          Some(data) => Ok(data),
+          None => Err(ErrorEnum::CustomError(String::from("user not exist")))
+        }
+      },
+      Err(_) => Err(ErrorEnum::InternalServerError)
+    }
 }
 
-#[doc = "Update verified status"]
-pub async fn update_verified_status(pool: Pool<Postgres>, email: String) -> anyhow::Result<bool> {
+#[doc = "Update an existing user data."]
+pub async fn update_user_data(pool: Pool<Postgres>, email: String) -> anyhow::Result<bool, ErrorEnum> {
   match sqlx::query!(
     "update users set verified = true where email = $1",
     email)
-    .execute(&pool)
+    .fetch_optional(&pool)
     .await {
-      Ok(_) => Ok(true),
-      Err(_) => Ok(false)
+      Ok(optional_data) => {
+        match optional_data {
+          Some(_) => Ok(true),
+          None => Ok(false)
+        }
+      },
+      Err(_) => Err(ErrorEnum::InternalServerError)
     }
 }
--- src/bin/auth_service/repositories/otp_repository.rs
+++ src/bin/auth_service/repositories/otp_repository.rs
@@ -1,42 +1,36 @@
-use serde_json::Value;
 use sqlx::{Pool, Postgres};
 
-use crate::models::otp_model::*;
+use crate::{
+  models::otp_model::*,
+  schemas::otp_schema::*,
+};
 
-use scholarmate_api::helpers::parse::convert_vec_to_values;
+use scholarmate_api::enums::error_enum::ErrorEnum;
 
-#[doc = "Fetch otp data by token"]
-pub async fn fetch_otp_data_by_token(pool: Pool<Postgres>, token: String) -> Option<OTPModel> {
-  let data = sqlx::query_as!(OTPModel, "select * from tokens where token = $1", token)
-    .fetch_optional(&pool)
-    .await
-    .unwrap();
-
-  data
-}
-
-#[doc = "Insert new token for otp"]
-pub async fn insert_otp_data(pool: Pool<Postgres>, token: String, otp: String, email: String) -> anyhow::Result<bool> {
-  match sqlx::query!(
-    "insert into tokens (token, otp, email) values ($1, $2, $3)",
-    token, otp, email)
-    .execute(&pool)
+#[doc = "Create a token data."]
+pub async fn create_token_data(pool: Pool<Postgres>, body: OTPVerificationSchema) -> anyhow::Result<OTPModel, ErrorEnum> {
+  match sqlx::query_as!(OTPModel,
+    "insert into tokens (token, otp, email) values ($1, $2, $3) returning *",
+    body.token, body.otp, body.email)
+    .fetch_one(&pool)
     .await {
-      Ok(_) => Ok(true),
-      Err(_) => Ok(false)
+      Ok(data) => Ok(data),
+      Err(_) => Err(ErrorEnum::InternalServerError)
     }
 }
 
-#[doc = "Delete otp data by token"]
-pub async fn delete_otp_data(pool: Pool<Postgres>, token: String) -> Vec<Value> {
-  let data = sqlx::query_as!(OTPModel,
-    "delete from tokens where token = $1 returning *",
-    token)
-    .fetch_all(&pool)
-    .await
-    .unwrap();
-
-  let result = convert_vec_to_values(data);
-
-  result
+#[doc = "Delete token data by token"]
+pub async fn delete_token_data(pool: Pool<Postgres>, token: String, email: String) -> anyhow::Result<OTPModel, ErrorEnum> {
+  match sqlx::query_as!(OTPModel,
+    "delete from tokens where token = $1 or email = $2 returning *", token, email)
+    .fetch_optional(&pool)
+    .await {
+      Ok(optional_data) => {
+        match optional_data {
+          Some(data) => Ok(data),
+          None => Err(ErrorEnum::CustomError(String::from("token not exist")))
+        }
+      },
+      Err(_) => Err(ErrorEnum::InternalServerError)
+    }
 }
